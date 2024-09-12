d64fdbbb72eea711c4dc15956013391db791db3f
ashaluba <mkvlb@mail.ru>
2024-09-12 15:20:15 +0300

    начало

---
+++ laba1/laba1/laba1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int option;
+    cout << "1. Добавить трубу" << endl
+        << "2. Добавить КС" << endl
+        << "3. Просмотр всех объектов" << endl
+        << "4. Редактировать трубу" << endl
+        << "5. Редактировать КС" << endl
+        << "6. Выход" << endl;
+    cin >> option;
+    
+    switch (option)
+    {
+    case 1: //add pipe
+    {
+        cout << "hdhd" << endl;
+        break;
+    }
+    case 2: //add cs
+    {
+        cout << "iwed" << endl;
+        break;
+    }
+    case 3: //edit pipe
+    {
+        cout << "jdku" << endl;
+        break;
+    }
+    case 4: //edit cs
+    {
+        cout << "jeiw" << endl;
+        break;
+    }
+    case 5: //save
+    {
+        cout << "slwo" << endl;
+        break;
+    }
+    }
+
+}
+
+
+
+
